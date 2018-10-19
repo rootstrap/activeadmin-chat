0a931ee037090f18bda0eab05c204f6c6e400e0c
Santiago Bartesaghi <santiago.bartesaghi@rootstrap.com>
2018-10-19 13:20:24 -0300

    Configure ActiveAdmin in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Setup ActiveAdmin
+ActiveAdmin.application.load_paths = [File.expand_path('../dummy/app/admin', __FILE__)]
+ActiveAdmin.unload!
+ActiveAdmin.load!
+Rails.application.reload_routes!
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
