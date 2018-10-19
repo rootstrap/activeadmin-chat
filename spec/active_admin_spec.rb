1feb853813bc9848ea7541018f4845d207986d9a
Santiago Bartesaghi <santiago.bartesaghi@rootstrap.com>
2018-10-19 11:22:50 -0300

    Add register_chat to ActiveAdmin

---
+++ spec/active_admin_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe ActiveAdmin do
+  context ".register_chat" do
+    it "calls application's #register_page" do
+      subject { ActiveAdmin }
+      expect(ActiveAdmin.application).to receive(:register_page).with("My Page", { namespace: "public" })
+
+      subject.register_chat "My Page", namespace: "public"
+    end
+  end
+end
