668fd5ba75699bc8f29cf1ba1fd6ca5f44eacd33
Federico Aldunate <federico.aldunatec@gmail.com>
2018-11-08 15:14:36 -0300

    PR fixes and recommendations

--- spec/helpers/chat_helper_spec.rb
+++ spec/helpers/chat_helper_spec.rb
@@ -16,10 +16,27 @@ describe ChatHelper, type: :helper do
       expect(helper.admin_class(text)).to eq('admin')
     end
 
-    it "returns nil if message sender is not an admin" do
+    it 'returns nil if message sender is not an admin' do
       text = create(:text, sender: person, conversation: conversation)
 
-      expect(helper.admin_class(text)).to eq(nil)
+      expect(helper.admin_class(text)).to be_nil
+    end
+  end
+
+  describe '#selected_class' do
+    let!(:admin_user) { create(:admin_user) }
+    let!(:person1) { create(:person) }
+    let!(:person2) { create(:person) }
+    let!(:conversation1) { create(:conversation, person: person1) }
+    let!(:conversation2) { create(:conversation, person: person2) }
+
+    it "returns 'selected' if both conversations are the same one" do
+
+      expect(helper.selected_class(conversation1, conversation1)).to eq('selected')
+    end
+
+    it 'returns nil if conversations are different' do
+      expect(helper.selected_class(conversation1, conversation2)).to be_nil
     end
   end
 end
