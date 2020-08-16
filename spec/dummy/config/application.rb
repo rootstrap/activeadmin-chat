7b2cc99e5db2a4bb453ad5744feba4874293b308
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2020-08-16 19:51:50 -0300

    Fix deprecation warning

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -24,5 +24,7 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
